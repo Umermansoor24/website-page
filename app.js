d7ec162ac58e1bb98a74ffb0a48526180f906169
Umermansoor24 <155991287+Umermansoor24@users.noreply.github.com>
2024-01-21 21:55:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+let age = prompt('Whats Your Age ?')
+
+console.log('named')
+console.log('age')
+
+
+if (age <= 25){
+    console.log('Your Are Student');
+}
+
+else{
+    console.log('You Are Not Student');
+}
+let userName = prompt('Whats Your Name?')
+alert(   'Welcome to  My WebPage' )
